c54262239ade09a3168c872b8e57f8c4f8bb437c
Topdev <toptal.alltime@gmail.com>
2020-11-04 22:09:07 -0500

    packages

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("./settings");
 const Case = require("./api/cases/model");
-
+const path = require('path');
 // import CaseService from './api/cases/routes';
 
 const { ObjectId } = mongoose.Types;
